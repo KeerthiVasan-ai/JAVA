b1983fd8745728a92d3ba1d2cd18518d065afbba
ragu8 <ragumdr2023@gmail.com>
2022-09-04 11:11:19 +0530

    var& dT

--- Basics/src/varz.java
+++ Basics/src/varz.java
@@ -1,7 +1,13 @@
+import java.io.*;
+import java.lang.*;
 public class varz{
 public static void main(String args[]){
- int Price;
+ int Price,Amount;
+ double sys;
  Price = 12;
+ sys=23;
  System.out.println(Price);
+ System.out.println(sys);
+
  }
  }
